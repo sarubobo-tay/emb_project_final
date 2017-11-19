a2ccf490a9fc28f34f212c4af5ce70310fbe2361
Class Account <ee106a-adp@imail.EECS.Berkeley.EDU>
2017-11-19 14:20:59 -0800

    First commit

---
+++ final_project/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa17/class/ee106a-adp/emb_workspace/final_project/src
+# Build directory: /home/cc/ee106a/fa17/class/ee106a-adp/emb_workspace/final_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(mapping)
